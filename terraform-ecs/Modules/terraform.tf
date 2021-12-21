09e66b05401f1a822f4091b5e7a62493c1342807
Madhu Yalamanchili <myalamanchili@macbook-pro.lan>
2021-12-21 10:22:58 -0700

    testing

--- terraform-ecs/Modules/terraform.tf
+++ terraform-ecs/Modules/terraform.tf
@@ -227,20 +227,16 @@ EOF
 
   execution_role_arn = aws_iam_role.hw_example_task_execution_role.arn
 
-  # These are the minimum values for Fargate containers.
+
   cpu                      = 256
   memory                   = 512
   requires_compatibilities = ["FARGATE"]
 
-  # This is required for Fargate containers (more on this later).
+
   network_mode = "awsvpc"
 }
 
-# This is the role under which ECS will execute our task. This role becomes more important
-# as we add integrations with other AWS services later on.
 
-# The assume_role_policy field works with the following aws_iam_policy_document to allow
-# ECS tasks to assume this role we're creating.
 resource "aws_iam_role" "hw_example_task_execution_role" {
   name               = "shw_example-task-execution-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
@@ -257,13 +253,12 @@ data "aws_iam_policy_document" "ecs_task_assume_role" {
   }
 }
 
-# Normally we'd prefer not to hardcode an ARN in our Terraform, but since this is an AWS-managed
-# policy, it's okay.
+
 data "aws_iam_policy" "ecs_task_execution_role" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# Attach the above policy to the execution role.
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.hw_example_task_execution_role.name
   policy_arn = data.aws_iam_policy.ecs_task_execution_role.arn
